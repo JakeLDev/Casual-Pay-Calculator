cc746d8fdefd5d4c75b98e49f08fbdcf82510f8b
JakeLDev <me@jakelyell.dev>
2022-09-12 17:42:59 +0800

    Gcal updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
+import { selectHasToken } from './stores/authentication';
+import store from './stores';
+
+
 import './App.css';
 import InputForm from './components/InputForm/InputForm';
 import TotalPay from './components/TotalPay/TotalPay';
 
 
 
-function App() {
+const App = () => {
 
-  // var hasToken = useSelector(selectHasToken);
-  var hasToken = false;
+  const hasToken = useSelector(selectHasToken);
+  // console.log(hasToken);
+  // var hasToken = false;
   const [calendar, setCalendar] = React.useState(false);
   // var begin = false;
   const [begin, setBegin] = React.useState(false);
@@ -35,34 +40,36 @@ function App() {
   }
 
   return (
-    <div className="App p-5 h-screen dark:bg-zinc-800 dark:text-gray-400">
-      <div className="Container">
-        <h1 className="text-4xl font-bold m-2 text-gray-300">  
-          Casual Pay Calculator
-        </h1>
-        <div className="flex flex-col items-center max-w-sm mx-auto my-2 text-inherit">
-            <p className='text-inherit'> You may enter your shifts automatically, or you may allow this site to read your Google Calender and use the events saved instead.</p>
-            <button className="bg-cyan-600 hover:bg-cyan-800 text-white font-bold my-2 py-2 px-4 rounded-full visible" id='StartButton' onClick={start}>
-                Begin
-            </button>
-            <button className="bg-cyan-600 hover:bg-cyan-800 text-white font-bold my-2 py-2 px-4 rounded-full hidden" id='CalendarToggle' onClick={toggleCalendar}>
-                Read Google Calender
-            </button>
-            {/* <button className="bg-cyan-600 hover:bg-cyan-800 text-white font-bold my-2 py-2 px-4 rounded-full" onClick={selectManual}>
-                Enter Shifts Manually
-            </button>
-            <button className="bg-cyan-600 hover:bg-cyan-800 text-white font-bold py-2 px-4 rounded-full" onClick={selectCalendar}>
-                Read Google Calender
-            </button> */}
-        </div>
-        {/* {!hasToken && !manual && <AuthScreen manual={manual}/>} */}
-        {/* {!manual && <CalendarInterface />} */}
-        {/* {!hasToken && manual && <ManualInterface manual={manual}/>} */}
-        {begin && <InputForm manual={!calendar}/>}
-        <TotalPay />
+    <Provider store={store()}>
+      <div className="App p-5 h-screen dark:bg-zinc-800 dark:text-gray-400">
+        <div className="Container">
+          <h1 className="text-4xl font-bold m-2 text-gray-300">  
+            Casual Pay Calculator
+          </h1>
+          <div className="flex flex-col items-center max-w-sm mx-auto my-2 text-inherit">
+              <p className='text-inherit'> You may enter your shifts automatically, or you may allow this site to read your Google Calender and use the events saved instead.</p>
+              <button className="bg-cyan-600 hover:bg-cyan-800 text-white font-bold my-2 py-2 px-4 rounded-full visible" id='StartButton' onClick={start}>
+                  Begin
+              </button>
+              <button className="bg-cyan-600 hover:bg-cyan-800 text-white font-bold my-2 py-2 px-4 rounded-full hidden" id='CalendarToggle' onClick={toggleCalendar}>
+                  Read Google Calender
+              </button>
+              {/* <button className="bg-cyan-600 hover:bg-cyan-800 text-white font-bold my-2 py-2 px-4 rounded-full" onClick={selectManual}>
+                  Enter Shifts Manually
+              </button>
+              <button className="bg-cyan-600 hover:bg-cyan-800 text-white font-bold py-2 px-4 rounded-full" onClick={selectCalendar}>
+                  Read Google Calender
+              </button> */}
+          </div>
+          {/* {!hasToken && !manual && <AuthScreen manual={manual}/>} */}
+          {/* {!manual && <CalendarInterface />} */}
+          {/* {!hasToken && manual && <ManualInterface manual={manual}/>} */}
+          {begin && <InputForm manual={!calendar}/>}
+          <TotalPay />
 
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
---
+++ src/constants.ts
@@ -0,0 +1,19 @@
+export const RANGE_TYPE = {
+    DAY: 'day',
+    WEEK: 'week',
+    MONTH: 'month',
+    YEAR: 'year',
+    TOTAL: 'total',
+    CUSTOM: 'custom',
+  };
+  
+  export const SORT_BY = {
+    DATE: 'date',
+    AMOUNT: 'amount',
+  };
+  
+  export const WEEK_START = {
+    MONDAY: 'monday',
+    SUNDAY: 'sunday',
+  };
+  
