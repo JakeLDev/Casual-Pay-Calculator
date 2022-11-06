bb2550619c3022fbacca6b0dab96bcd19bf1847d
JakeLDev <me@jakelyell.dev>
2022-11-06 20:55:45 +0800

    Update getAllEvents.ts

--- src/Functions/getAllEvents.ts
+++ src/Functions/getAllEvents.ts
@@ -8,13 +8,6 @@ export function getAllEvents(apiCalendar: any) {
                             orderBy: 'startTime'},
                             calendarId)
                             .then(({ result }: any) => {
-        // Events = result.items;
         sessionStorage.setItem('Events', JSON.stringify(result.items));
-        // console.log(sessionStorage.getItem('Events'));
-        // console.log(JSON.parse(sessionStorage.getItem('Events') || '[]'));
-        // console.log(JSON.parse(sessionStorage.Events));
-        // document.getElementById("GetEvents")!.className = "hidden";
-        console.log("Events Fetched!");
-        // console.log(result.items); // This is an array of 500 events from the start of 2021 to a year from now
     }); //TODO catch errors
  }; //TODO throw alert if user tries to filter events before fetching them
