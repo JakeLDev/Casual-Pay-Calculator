dd3f574d4a0d5387ebc2271ecb011b3f43acddc9
JakeLDev <me@jakelyell.dev>
2022-11-06 20:06:01 +0800

    Update createTimeRange.ts

--- src/Functions/createTimeRange.ts
+++ src/Functions/createTimeRange.ts
@@ -2,7 +2,6 @@ export function createTimeRange(dates: string[]) {
     // get range from the selct field, create start and end dates or use the custom field
     var dateSelection = document.getElementById("timePeriod") as HTMLSelectElement;
     var selection = dateSelection.value; // "Year", "Month", "Week", "Day", "Total", "Custom"
-    console.log(selection);
     // if custom is selected, show the date picker
     
     var rangeStart = new Date();
@@ -10,7 +9,9 @@ export function createTimeRange(dates: string[]) {
 
     if (selection === "Day")    {
         rangeStart = new Date();
+        rangeStart.setHours(0,0,0,0);
         rangeEnd = new Date();
+        rangeEnd.setHours(23,59,59,999);
         document.getElementById("datePicker")!.className = "hidden";
     } else if (selection === "Week") {
         rangeStart = new Date();
@@ -47,12 +48,10 @@ export function createTimeRange(dates: string[]) {
         // show the date picker
         var datePicker = document.getElementById("datePicker");
         datePicker!.className = "visible";
-        // console.log("Dates", dates)
         if (dates[0].length === 2) {
             rangeStart = new Date(dates[0][0]);
             rangeEnd = new Date(dates[0][1]);
         }
     }
-    console.log(rangeStart, rangeEnd);
     return [rangeStart, rangeEnd];
 }
