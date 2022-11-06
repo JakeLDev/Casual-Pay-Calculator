998356c677d15587e3158933d4b42c5c29a0830d
JakeLDev <me@jakelyell.dev>
2022-11-06 20:27:19 +0800

    Update sumHours.ts

--- src/Functions/sumHours.ts
+++ src/Functions/sumHours.ts
@@ -6,7 +6,6 @@ export function sumHours(filteredList: any[]) {
     var eveningHours = 0;
     var overtimeHours = 0;
     filteredList.forEach((event: any) => {
-        console.log(event);
         weekdayHours += event.weekdayHours;
         saturdayHours += event.saturdayHours;
         sundayHours += event.sundayHours;
@@ -14,13 +13,6 @@ export function sumHours(filteredList: any[]) {
         eveningHours += event.eveningHours;
         overtimeHours += event.overtimeHours;
     });
-    console.log("sum hours");
-    console.log(weekdayHours);
-    console.log(saturdayHours);
-    console.log(sundayHours);
-    console.log(publicHolidayHours);
-    console.log(eveningHours);
-    console.log(overtimeHours);
     return {
         weekdayHours: weekdayHours,
         saturdayHours: saturdayHours,
